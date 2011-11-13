a86aa1352499239d5d7eebcea0c1edca71f919c4
Ming Chow <mchow@cs.tufts.edu>
2011-11-13 00:00:27 -0500

    Updated Twitter search program

--- php/twitter_search.php
+++ php/twitter_search.php
@@ -1,7 +1,20 @@
+<form method="post">
+	<p>Search <input type="text" name="term" /> <input type="submit"/></p>
+</form>
+
 <?php
-	$search = "CIA hacked";
-	$url = "http://search.twitter.com/search.json?q=" . urlencode($search);
-	$json = file_get_contents($url);
-	$parsed = json_decode($json);
-	print_r($parsed);
+	$input = $_POST['term'];
+	$whitelist = array('ndfootball', 'FightingIrishND', 'NotreDameCFB', 'KeithArnoldNBC', 'uhnd');
+	$api = 'http://search.twitter.com/search.json?q=';
+	foreach ($whitelist as $i) {
+		$results = file_get_contents($api . $i);
+		$json = json_decode($results);
+		foreach ($json->{'results'} as $r) {
+			$text = $r->{'text'};
+			$text = preg_replace("/([\w]+:\/\/[\w-?&;#~=\.\/\@]+[\w\/])/i","<a href=\"$1\" target=\"_blank\">$1</a>", $text);
+			if (preg_match("/$input/i", $text)) {
+				print "<p><strong>" . $i . "</strong> - " . $text . "</p>";
+			}
+		}
+	}
 ?>
